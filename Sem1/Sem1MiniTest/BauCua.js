6cefb06227ffd8f1a7212bc313e5c412477a2702
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-03-30 00:10:16 +0700

    Sem 1 Mini Test: Bau Cua with Randomizer Done

--- Sem1/Sem1MiniTest/BauCua.js
+++ Sem1/Sem1MiniTest/BauCua.js
@@ -1,9 +1,34 @@
-// random shuffle function
+let counter = 0;
 
-// function Shuffle () {
-//     document.getElementsByClassName("myImg").src = Math.random()*5 +1
-// }
+const images = ["./assets/bau.png",
+    "./assets/cua.png",
+    "./assets/tom.png",
+    "./assets/ca.png",
+    "./assets/huou.png",
+    "./assets/ga.png"];
 
-function Shuffle () {
-    document.getElementsByClassName("myImg").src = Math.random()*5+1
+function randomNumber(){
+    let number = Math.floor(Math.random() * 6);
+    return number
 }
+
+function Shuffle() {
+    // document.getElementsByClassName("myImg").src = images[number];
+    document.getElementById("Img1").src = images[randomNumber()];
+    document.getElementById("Img2").src = images[randomNumber()];
+    document.getElementById("Img3").src = images[randomNumber()];
+}
+
+
+// Testing zone
+function ShuffleInterval() {
+    Shuffle();
+    intervalID = setInterval(Shuffle, 30);
+
+    setTimeout(() => {
+        clearInterval(intervalID); // Clear the interval
+        console.log("Shuffling stopped after 3 seconds.");
+    }, 3000);
+}
+
+
