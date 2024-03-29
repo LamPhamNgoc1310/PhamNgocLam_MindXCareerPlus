7105b161ec069b135cfc98a12ae860e88b3112df
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-03-29 21:57:25 +0700

    Updated with MiniTest, lecture 13, 14

---
+++ Sem1/13_Bootstrap/bootstrap.js
@@ -0,0 +1,12 @@
+// anon function
+const sum = function(a,b) {
+    return a + b;
+}
+
+// arrow function
+const result = (a,b) => {
+    const result = a + b;
+    return result;
+}
+
+// hoisting.
---
+++ Sem1/Sem1MiniTest/BauCua.js
@@ -0,0 +1,9 @@
+// random shuffle function
+
+// function Shuffle () {
+//     document.getElementsByClassName("myImg").src = Math.random()*5 +1
+// }
+
+function Shuffle () {
+    document.getElementsByClassName("myImg").src = Math.random()*5+1
+}
